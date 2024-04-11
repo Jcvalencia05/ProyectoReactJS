b3baee7ef7ae3db1ae8777190bd93840e07de2e0
Juan Valencia <148996370+Jcvalencia05@users.noreply.github.com>
2024-04-11 01:30:01 -0500

    Delete proyecto-app-main/src/utils directory

--- proyecto-app-main/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-const baseUrl = "https://fakestoreapi.com";
-
-export const getData = async (url) => {
-  const data = await fetch(baseUrl + url);
-  const json = await data.json();
-  return json;
-};
