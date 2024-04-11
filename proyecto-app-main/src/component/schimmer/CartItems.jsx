24d358b31c572b9f2ffc831d41ab4f3f7dbcdd09
Juan Valencia <148996370+Jcvalencia05@users.noreply.github.com>
2024-04-11 01:31:09 -0500

    Delete proyecto-app-main/src/component directory

--- proyecto-app-main/src/component/schimmer/CartItems.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function CartItems() {
-  return (
-    <main className=" h-[100vh] w-full flex justify-center items-center">
-      <h1 className="font-bold text-2xl">El Carro Esta Vacio</h1>
-    </main>
-  );
-}
